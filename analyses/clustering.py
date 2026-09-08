6d4bc294dffa751fae1fb5775a01376cd3618691
alopilato88 <alex.lopilato@gmail.com>
2026-09-08 05:08:14 -0400

    Replicate segmentation R script (CH3) in Python.

--- analyses/clustering.py
+++ analyses/clustering.py
@@ -1,5 +1,27 @@
 import pandas as pd
 import sklearn as sk
+import numpy as np
 from sklearn.cluster import KMeans
 
+# Set the seed for reporducibility
+np.random.seed(1)
+
+# Read data 
 data_cluster = pd.read_csv("data/Chapter 3/retail_segmentation.csv")
+
+# Create predictor data
+pred_names = ["avg_order_size", "avg_order_freq", "crossbuy", "multichannel", "per_sale", 
+               "tenure", "avg_mktg_cnt", "return_rate"]
+
+data_x = data_cluster[pred_names]
+
+# Instantiate KMeans model
+model_k_6 = KMeans(n_clusters = 6)
+
+model_k_6.fit(data_x)
+
+# Add cluster ids back to data_cluster
+data_cluster["cluster_id"] = model_k_6.labels_
+
+# Look at cluster means
+data_cluster.groupby("cluster_id")["avg_order_size"].mean()
